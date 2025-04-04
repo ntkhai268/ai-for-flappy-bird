f9d9b7ba6906bae5caa1f09617626655885e8c4b
Thanh Khai Nguyen <khainguyen3626@gmail.com>
2025-04-04 08:32:41 +0700

    first commit

---
+++ generatic.py
@@ -0,0 +1,14 @@
+class Chromosome:
+    """
+    Tạo một class Chromosome (nhiễm sắc thể) để tạo và lấy những trạng thái
+    Chromosome có dạng: (x0, y0, v)
+    """
+    def __init__(self):
+       self.gens = 
+    pass
+
+class Population:
+    pass
+
+class GeneticAlgorithm:
+    pass
